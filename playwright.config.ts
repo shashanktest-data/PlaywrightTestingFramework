3e5d268537865810313368535411474f0eaddd2a
shashanktest-data <ershashank.iimt@gmail.com>
2026-04-28 22:46:15 +0530

    final test fixed

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
      //storageState:"./data/loginstate.json",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'off',
     video:"retain-on-failure",
     screenshot:"only-on-failure",
     headless: true,
